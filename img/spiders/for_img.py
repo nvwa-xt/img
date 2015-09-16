f8e675f9aa3a3fa6d1ab752fd084695ebaf5ba12
admin <admin@localhost.localdomain>
2015-09-16 08:58:50 +0800

    第一次提交，可基本下载特定网页特定图片

---
+++ img/spiders/for_img.py
@@ -0,0 +1,16 @@
+import scrapy
+import urllib
+import re
+
+class ImgSpider(scrapy.Spider):
+    name = "forimg"
+    allowed_domains = ["tp123456.com"]
+    start_urls = ["http://img.tp123456.com/Html/53964.html",]
+
+    def parse(self,response):
+        url = response.xpath('//img/@src').extract()
+        x = 0
+        for imgurl in url:
+            imgurl = 'http://img.tp123456.com'+imgurl
+            urllib.urlretrieve(imgurl,'%s.jpg' %x )
+            x+=1
